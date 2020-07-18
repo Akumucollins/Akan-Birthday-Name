4b0e7d138c8ae2be2ecefc73b963b24a06dda5ae
Akumu collins <akumucollins001@gmail.com>
2020-07-18 09:37:57 +0300

    created an array for months

--- js/styles.js
+++ js/styles.js
@@ -14,4 +14,16 @@ function array(n){
  days[5] = ''  
  days[6] = ''
  
- 
+ months = new array(12);
+ months[1] = 'January'
+ months[2] = 'February'
+ months[3] = 'March'
+ months[4] = 'April'
+ months[5] = 'May'
+ months[6] = 'June'
+ months[7] = 'July'
+ months[8] = 'August'
+ months[9] = 'September'
+ months[10] = 'October'
+ months[11] = 'November'
+ months[12] = 'December'
