6bac820c7fc97023aa0483d24651e0b0f86a2bd6
Akumu collins <akumucollins001@gmail.com>
2020-07-18 09:30:19 +0300

    added a function in the styles.js and created days of the week

---
+++ js/styles.js
@@ -0,0 +1,17 @@
+function array(n){
+        let.lenght = n;
+        for(var i=1; i<=n; i++){
+            let[i]=0
+        }
+    }
+
+ days = new array[7];
+ days[0] = ''  
+ days[1] = ''  
+ days[2] = ''  
+ days[3] = ''  
+ days[4] = ''  
+ days[5] = ''  
+ days[6] = ''
+ 
+ 
