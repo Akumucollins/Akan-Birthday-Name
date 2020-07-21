399abedcbb2869b10f35fd751308296dec4bda02
Akumu collins <akumucollins001@gmail.com>
2020-07-21 19:03:30 +0300

    created description comment-out text

--- js/styles.js
+++ js/styles.js
@@ -1,9 +1,11 @@
+// variables
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+// Validation function
 
 function additionSubmit() {
     event.preventDefault();
